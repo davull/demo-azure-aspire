24864d05499cf41355a12334d5068fcd728db382
David Ullrich <david.ullrich@ullrich-software.de>
2024-11-27 20:40:16 +0100

    Add Redis client to ApiService

--- src/AspireStarterDemo.ApiService/Program.cs
+++ src/AspireStarterDemo.ApiService/Program.cs
@@ -1,3 +1,6 @@
+using System.Text.Json;
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add service defaults & Aspire client integrations.
@@ -9,6 +12,10 @@ builder.Services.AddProblemDetails();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Add Redis client
+// https://learn.microsoft.com/en-us/dotnet/aspire/caching/stackexchange-redis-integration#client-integration
+builder.AddRedisClient(connectionName: "cache");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,21 +26,28 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-string[] summaries = ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
+string[] summaries =
+    ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapGet("/weatherforecast", async (IConnectionMultiplexer redis) =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+            new WeatherForecast
+            (
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Random.Shared.Next(-20, 55),
+                summaries[Random.Shared.Next(summaries.Length)]
+            )).ToArray();
+
+        var db = redis.GetDatabase();
+
+        var key = $"{Random.Shared.Next(0, 10)}";
+        var value = JsonSerializer.Serialize(forecast);
+        await db.StringGetSetAsync(key, value);
+
+        return value;
+    })
+    .WithName("GetWeatherForecast");
 
 app.MapDefaultEndpoints();
 
